779952eac242d0be9115cb538964b9631bc59510
arsalanrez <arsalanrezazadeh@hotmail.com>
2024-05-09 14:42:30 +0330

    js file modified

---
+++ analog_clock/app.js
@@ -0,0 +1,19 @@
+const secondHand = document.getElementById("seconds");
+const minutesHand = document.getElementById("minutes");
+const hourshand = document.getElementById("hours");
+
+const getTime = () => {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+    const timeInterval = 6;
+    console.log(`${hours}:${minutes}:${seconds}`);
+
+    secondHand.style.transform = "rotate(" + seconds * timeInterval + "deg)";
+    minutesHand.style.transform =
+        "rotate(" + (minutes * timeInterval) / 10 + "deg)";
+    hourshand.style.transform = "rotate (" + (hours * 30 + minutes/ 2) + "deg)";
+};
+
+setInterval(getTime, 100);
